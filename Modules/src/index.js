6868e8dad75ebb70686044cda012ee3df22caa73
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-29 15:55:30 -0300

    Modules
    Modules

---
+++ Modules/src/index.js
@@ -0,0 +1,10 @@
+// import {nome, sobrenome, idade, soma} from  './modulo1.js'
+
+
+
+// console.log(nome, sobrenome, idade )
+
+// console.log(soma(5, 5))
+
+import * as MeuModulo from './modulo1.js'
+console.log(MeuModulo)
---
+++ Modules/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome =`Ednardo`;
+const sobrenome = `Paz`;
+const idade = 37;
+
+function soma( x, y ) {
+    return (x + y)
+};
+
+export {nome, sobrenome, idade, soma}
